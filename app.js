129a8034d12f234b5b9c4ae65a287095128726ad
Robert Noack <robertnoack4@gmail.com>
2022-06-12 16:06:11 +0200

    Added Option to get Geo Data from other API

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 import request from 'postman-request';
 
-const url = "http://api.weatherstack.com/current?access_key=b75b0f4617abdccbe9a5c9d825a7bb97&query=37.8267,-122.4233&units=m"
+const urlGeoCoding = "http://api.openweathermap.org/geo/1.0/direct?q=Dresden,de&limit=1&appid=e0387cf96f96fa0fb36a6bd9fe5cc754"
 
-request({url: url, json: true}, (error, response)=> {
+request({url: urlGeoCoding, json: true}, (error, response)=>{
+    console.log(response.body)
+})
+
+const urlWeather = "http://api.weatherstack.com/current?access_key=b75b0f4617abdccbe9a5c9d825a7bb97&query=37.8267,-122.4233&units=m"
+
+request({url: urlWeather, json: true}, (error, response)=> {
     console.log("It is currently " + response.body.current.temperature + " degrees out. It feels like " + response.body.current.feelslike + " degrees")
 })
