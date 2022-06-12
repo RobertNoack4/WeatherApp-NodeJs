a6129a950b54722cb9441400857b04ef07aecc33
Robert Noack <robertnoack4@gmail.com>
2022-06-12 15:28:43 +0200

    Updated request

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ import request from 'postman-request';
 
 const url = "http://api.weatherstack.com/current?access_key=b75b0f4617abdccbe9a5c9d825a7bb97&query=37.8267,-122.4233"
 
-request({url: url}, (error, response)=> {
-    const data = JSON.parse(response.body)
-    console.log(data.current)
+request({url: url, json: true}, (error, response)=> {
+    console.log(response.body.current)
 })
